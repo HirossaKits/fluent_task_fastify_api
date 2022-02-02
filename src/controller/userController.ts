4b65f8517e46a9d5e668b5d4eabc97984066c546
kits-create <hi.kitsuka@gmail.com>
2022-02-02 12:59:12 +0900

    ユーザー更新削除処理追加

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -24,3 +24,25 @@ export const addUser = async (req: any, reply: any) => {
     reply.status(500).send(error);
   }
 };
+
+export const updateUser = async (req: any, reply: any) => {
+  try {
+    const user_id = req.params;
+    const user = await prisma.user.update({
+      where: { use_id: user_id },
+      data: req.body,
+    });
+    reply.send(user);
+  } catch (error) {
+    reply.status(500).send(error);
+  }
+};
+
+
+export const deleteUser = async(req:any, reply:any) => {
+  try{
+    const {user_id} = req.params;
+    const user = await prisma.user.delete({where:{user_id:user_id}})
+    reply.send(user);
+  }
+}
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,5 +1,10 @@
 // import fastify from "fastify"
-import { getUser, addUser } from '../../controller/userController';
+import {
+  getUser,
+  addUser,
+  updateUser,
+  deleteUser,
+} from '../../controller/userController';
 
 const User = {
   user_id: { type: 'string' },
@@ -36,9 +41,33 @@ const postUserOpts = {
   handler: addUser,
 };
 
+const updateUserOpts = {
+  schema: {
+    param: {
+      org_id: { type: 'string' },
+      is_org_rep: { type: 'boolean' },
+      is_org_admin: { type: 'boolean' },
+    },
+  },
+  handler: updateUser,
+};
+
+const deleteUserOpts = {
+  schema: {
+    param: {
+      org_id: { type: 'string' },
+      is_org_rep: { type: 'boolean' },
+      is_org_admin: { type: 'boolean' },
+    },
+  },
+  handler: deleteUser,
+};
+
 const org = async (fastify: any, options: any, done: any) => {
   fastify.get('/:user_id', getUserOpts);
   fastify.post('/', postUserOpts);
+  fastify.put('/:user_id', updateUserOpts);
+  fastify.delete('/:user_id', deleteUserOpts);
   done();
 };
 
