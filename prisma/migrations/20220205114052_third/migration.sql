2f375c072d9d0e8e974d9532159d435c71922850
Hirohisa Kitsuka <kits@HirohisanoMac-mini.local>
2022-02-07 07:41:48 +0900

    フィルアップロード処理作成途中

---
+++ prisma/migrations/20220205114052_third/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "avatar_img" DROP NOT NULL;
---
+++ src/plugins/multipart.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin';
+
+export default fp(async (fastify, opts) => {
+  //   fastify.register(require('fastify-multipart'), {
+  //     limits: {
+  //       fieldNameSize: 100, // Max field name size in bytes
+  //       fieldSize: 100, // Max field value size in bytes
+  //       fields: 10, // Max number of non-file fields
+  //       fileSize: 1000000, // For multipart forms, the max file size in bytes
+  //       files: 1, // Max number of file fields
+  //       headerPairs: 20, // Max number of header key=>value pairs
+  //     },
+  //   });
+  fastify.register(require('fastify-multipart'));
+});
